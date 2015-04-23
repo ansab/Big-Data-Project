7fdf0e34bc8c6f65864d458d70cc9cab4526b366
solankikrunal <krunalsolanki999@gmail.com>
2015-04-22 18:14:59 -0700

    Revert "pressured includes"
    This reverts commit 792601a8603076e2d11c48562079b9215a9465d3.

--- Acquisition/Weather/weatherData.py
+++ Acquisition/Weather/weatherData.py
@@ -5,8 +5,8 @@ json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
-pressure = parsed_json['current_observation']['pressure_in']
+rain = parsed_json['current_observation']['pressure_in']
 print "Current temperature in %s is: %s" % (location, temp_f)
-print "Current pressure is: %s" % (pressure)
+print "Rainfall Measured is: %s" % (rain)
 
 f.close()
